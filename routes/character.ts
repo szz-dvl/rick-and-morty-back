bf2941bc6c586e7694596ac60d939b26f5672616
santizz <s.martinez@ondho.com>
2021-09-07 18:27:39 +0200

    episodes

--- routes/character.ts
+++ routes/character.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { needsToken } from "../strategies/jwt";
-import { getCharacter, getCharacters } from 'rickmortyapi';
+import { getCharacter, getCharacters, getEpisode } from 'rickmortyapi';
 import { StatusCodes } from "../constants";
 import { User } from "../models/user";
 import { getIp } from "../utils";
@@ -16,6 +16,19 @@ router.get('/list', async (req, res, next) => {
 
         const page = parseInt(req.query.page as string, 10) || 1;
         res.status(StatusCodes.OK).json({ characters: await getCharacters({ page }) });
+
+    } catch (err) {
+        next(err);
+    }
+
+});
+
+router.get('/episode/:id', async (req, res, next) => {
+
+    try {
+
+        const id = parseInt(req.params.id as string, 10);
+        res.status(StatusCodes.OK).json({ episode: await getEpisode(id) });
 
     } catch (err) {
         next(err);
